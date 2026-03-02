6f177cf817c49129a9a474e4e1b4a0744806bc6d
Maurits Schelvis <s1156856@student.hsleiden.nl>
2026-03-02 13:55:59 +0100

    initial commit

---
+++ src/main/java/homecinema/FirstFullStackBackendApplication.java
@@ -0,0 +1,13 @@
+package homecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstFullStackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstFullStackBackendApplication.class, args);
+    }
+
+}
